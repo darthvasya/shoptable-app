ab08a43949686f15105bfdd3dfea72745e4eeb90
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-09-15 00:58:32 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './dashboard/navbar/navbar.component';
+import { WrapperComponent } from './dashboard/wrapper/wrapper.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WrapperComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
